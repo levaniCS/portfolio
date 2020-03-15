0e206a803da6a8148a4d098ff36ee6ea81250290
levaniCS <levanisarishvili3105@gmail.com>
2020-03-15 20:22:26 +0400

    Updates

---
+++ precache-manifest.096deb4055b87ddd0e45ec6ce9b3d5c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30a15e6a6890377db00e62f1dd90f60f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7b6ba2a616a5e03c8d69",
+    "url": "/portfolio/static/css/main.2c95b0c7.chunk.css"
+  },
+  {
+    "revision": "1a4948698d9a53f8ad4f",
+    "url": "/portfolio/static/js/2.2972bf86.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.2972bf86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b6ba2a616a5e03c8d69",
+    "url": "/portfolio/static/js/main.b929f492.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "9ce8aa204a21e69716018fee472b042c",
+    "url": "/portfolio/static/media/app-dev.9ce8aa20.png"
+  },
+  {
+    "revision": "a8136515643d1bf44f566eed6d96973b",
+    "url": "/portfolio/static/media/bg2.a8136515.jpg"
+  },
+  {
+    "revision": "bab474e82cb181b5e32e0ddcf11d42c0",
+    "url": "/portfolio/static/media/me.bab474e8.jpg"
+  }
+]);
